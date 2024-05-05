3842c1651a87500e4b7f9cc3029d36e399b55a83
Volodymyr Antoniuk <voxtobox@gmail.com>
2024-05-05 16:40:58 -0700

    add print

--- task-2.py
+++ task-2.py
@@ -6,5 +6,12 @@ def get_is_palindrom(text):
   d = deque(normText)
   while len(d) > 1:
     if d.pop() != d.popleft():
+      print(f"{text} -> {normText} - не є паліндроном" )
       return False
+  print(f"{text} -> {normText} - є паліндроном")
   return True
+
+get_is_palindrom('L  f Tfl')
+get_is_palindrom('oanmao')
+get_is_palindrom('ppWWVV')
+get_is_palindrom('   nKL6mmm6 lkn')
